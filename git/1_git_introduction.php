a06f5a567c11f3290cf20a0229528b3e7111941a
resultisarkari <resultisarkari@gmail.com>
2021-06-06 13:03:50 +0530

    this is my test commit  1. what are you doing he  2. this is test

--- git/1_git_introduction.php
+++ git/1_git_introduction.php
@@ -1,1 +1,56 @@
-this is checking 
+==> `GIT Kraken` GUI of git .(https://www.gitkraken.com/)
+
+
+Command :
+--------
+
+open .     // opening current folder (for mac )
+start .    //  opening current folder (for windows )
+
+ls              // list file
+ls  -a          //  show hidden files 
+ls wordpress    // ye current directoty ke `wordpress` folder ko open kar dega 
+
+pwd             // return current directory location 
+
+touch red.txt orange.txt yellow.txt   // for generating 3 files in single line command 
+touch Pets/horses/rocky.png             // for generating `rocky.png` file inside horses folder 
+
+mkdir Plants                // generating folder `Plants`
+mkdir Plants  Animals              // generating folder `Plants` and  `animals`
+
+rm rocky.txt      // delete `rocky.txt` file 
+rm t1.tx t2.tx    // remove both files 
+rm -rf             // remove `directory`
+
+
+
+git  log  
+
+git init 
+
+git status 
+git status  -s      //gives information file is modified  or Untracked
+
+Exit :
+----
+q               ==> press `q` to exit from  log 
+
+
+Setting Custom code Editor (for git ):
+--------------------------------------
+git config --global core.editor 'code --wait' 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
