b09f6bd3b5ec640fc2516951c25e429e08deec1b
chandramani <mani@binarypoint.in>
2021-06-26 14:44:01 +0530

    wordpress debug page updates

--- wordpress/newSearchByMe/debug.php
+++ wordpress/newSearchByMe/debug.php
@@ -1,10 +1,16 @@
+Some Important plugin for Debuging: 
+-----------------------------------
+1) Simply Show Hooks    
+2) Debug Bar + Debug Bar Actions And filters Addon
+3) Using R Debug Library
+4) <?php
+    var_dump($wp_actions);
+    var_dump($wp_filter);
+?>
+5) Query Monitor
+
 /** for Development **/
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
 define( 'QM_ENABLE_CAPS_PANEL', true );
 define( 'QM_SHOW_ALL_HOOKS', true );
-
-
-
-
-// suna pada hai (song)  
