eecc89e9416bb78be8ab60463422bb59d9203d2a
Ravi <resultisarkari@gmail.com>
2022-03-30 08:54:53 +0530

    update

--- laravel/index.php
+++ laravel/index.php
@@ -1,1 +1,16 @@
 composer create-project --prefer-dist laravel/laravel:^7.0 demo 
+
+
+## add basic authentication with curl
+
+use Illuminate\Support\Facades\Http;
+
+
+$response = Http::withHeaders([
+        'Authorization' => 'Basic '. base64_encode("j7bFF1wFHpe1H05d:9VxIN0Xijs2gkLbsJ8efOzBxevcXE7vBSR7tFUAPdsVn3UDASFDv45S6oSRSNbhw"),
+    ])->get('https://api.patientsites.com/socmedfeedsapi.php', [
+        'action' => 'get_vars',
+    ]);
+
+
+
